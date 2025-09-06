69a9c1cc5f4277def3f008cb65337c36ccdb3687
HandofMbaise <tochinwachi23@gmail.com>
2025-09-06 12:22:14 -0700

    committed

--- flex.js
+++ flex.js
@@ -2,7 +2,13 @@ const box = document.querySelectorAll('.box');
 
 
 function Game(){
-   alert('Hi Box');
+   fetch('https://myheroku-app-b4ea51ca30c7.herokuapp.com/box', {
+      method: 'GET',
+      headers: {
+         'content-Type': 'text/plain'
+      }
+   }).then(response => response.text())
+   .then(data => window.alert(data))
 }
 
 box.forEach(box => {box.addEventListener('click', Game)});
